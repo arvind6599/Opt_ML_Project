a2b02859b15c5e16f5cecaaf019abf571b3f4287
arvind <arvind6599@gmail.com>
2024-06-14 01:20:30 +0200

    formatting error

--- utils.py
+++ utils.py
@@ -166,16 +166,16 @@ def deep_leakage_from_gradients(model, origin_grad):
 
     for iters in range(300):
         def closure():
-        optimizer.zero_grad()
-        dummy_pred = model(dummy_data) 
-        dummy_loss = criterion(dummy_pred, F.softmax(dummy_label, dim=-1)) 
-        dummy_grad = grad(dummy_loss, model.parameters(), create_graph=True)
+            optimizer.zero_grad()
+            dummy_pred = model(dummy_data) 
+            dummy_loss = criterion(dummy_pred, F.softmax(dummy_label, dim=-1)) 
+            dummy_grad = grad(dummy_loss, model.parameters(), create_graph=True)
 
-        grad_diff = sum(((dummy_grad - origin_grad) ** 2).sum() \
-            for dummy_g, origin_g in zip(dummy_grad, origin_grad))
-        
-        grad_diff.backward()
-        return grad_diff
+            grad_diff = sum(((dummy_grad - origin_grad) ** 2).sum() \
+                for dummy_g, origin_g in zip(dummy_grad, origin_grad))
+            
+            grad_diff.backward()
+            return grad_diff
         
         optimizer.step(closure)
         
